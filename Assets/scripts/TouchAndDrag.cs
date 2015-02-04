5e284c1fc3497d9c6fa5bdac5ab8225fa16be9ed
kmwhelan93 <kmw8sf@virginia.edu>
2015-02-04 11:29:46 -0500

    implemented touch and drag fixes #3

---
+++ Assets/scripts/TouchAndDrag.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchAndDrag : MonoBehaviour {
+	public float speed;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 1)
+		{
+			Debug.Log (camera.orthographicSize);
+			Vector2 deltaPosition = Input.GetTouch (0).deltaPosition;
+			Vector2 deltaToScreenRatio = deltaPosition * camera.orthographicSize / 5;
+			camera.transform.Translate(deltaToScreenRatio * -1 *speed);
+		}
+	}
+}
