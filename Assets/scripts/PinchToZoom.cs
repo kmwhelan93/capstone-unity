48698b3280ab3f324ac9ba467f2c721afc4b50b9
kmwhelan93 <kmw8sf@virginia.edu>
2015-02-04 11:06:00 -0500

    forced to landscape, added pinch to zoom on world view fixes #2

---
+++ Assets/scripts/PinchToZoom.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinchToZoom : MonoBehaviour {
+
+	public float orthoZoomSpeed;
+	public float perspectiveZoomSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 2)
+		{
+			Touch touchZero = Input.GetTouch(0);
+			Touch touchOne = Input.GetTouch (1);
+
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			float prevLength = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float currLength = (touchZero.position - touchOne.position).magnitude;
+
+			if (camera.isOrthoGraphic)
+			{
+				camera.orthographicSize += (currLength - prevLength) * orthoZoomSpeed;
+				camera.orthographicSize = Mathf.Max (0.1f, camera.orthographicSize);
+			} else
+			{
+				camera.fieldOfView += (currLength - prevLength) * perspectiveZoomSpeed;
+				camera.fieldOfView = Mathf.Clamp (camera.fieldOfView, 0.1f, 179.9f);
+			}
+		}
+	}
+}
