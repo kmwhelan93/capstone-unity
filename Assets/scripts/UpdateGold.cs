1281ea6d84b68f6270322147d412674291c66b38
kmwhelan93 <kmw8sf@virginia.edu>
2015-02-16 13:01:19 -0500

    added gold icon and label

---
+++ Assets/scripts/UpdateGold.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateGold : MonoBehaviour {
+
+	public GameObject goldText;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (syncGold ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+		public IEnumerator syncGold()
+		{
+			yield break;
+		}
+}
