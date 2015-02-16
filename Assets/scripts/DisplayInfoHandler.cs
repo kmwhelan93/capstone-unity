bd57b6141cdd56b4b923aa8dd0ae2f2f08659525
kmwhelan93 <kmw8sf@virginia.edu>
2015-02-16 11:12:38 -0500

    more intelligent label placing

--- Assets/scripts/DisplayInfoHandler.cs
+++ Assets/scripts/DisplayInfoHandler.cs
@@ -10,13 +10,73 @@ public class DisplayInfoHandler : MonoBehaviour {
 	public void positionText()
 	{
 		foreach(BaseWrapper bw in baseWrappers) {
-			GameObject baseObj = bw.baseObj;
-			float radius = baseObj.GetComponent<SphereCollider>().radius;
-			Vector3 worldOffset = new Vector3((float)radius*1.1f, (float)radius, 0);
+			Vector3 worldOffset = getBestOffset(bw);
 
 			Vector3 textPosition = Camera.main.WorldToScreenPoint(bw.baseObj.transform.position + worldOffset) - new Vector3(Screen.width / 2, Screen.height / 2, 0);
 			bw.displayText.transform.localPosition = textPosition;
 		}
 
+	}
+
+	private Vector3 getBestOffset(BaseWrapper bw)
+	{
+		GameObject baseObj = bw.baseObj;
+		GameObject displayText = bw.displayText;
+		Base b = baseObj.GetComponent<TouchBase>().b;
+		NESWBases nearBases = getNESWBases (b);
+		float radius = baseObj.GetComponent<SphereCollider>().radius;
+		if (nearBases.E == null) 
+		{
+			displayText.GetComponent<RectTransform>().pivot = new Vector2(0, .5f);
+			return new Vector3((float)radius*1.1f, (float)0, 0);
+		} 
+		else if (nearBases.W == null)
+		{
+			displayText.GetComponent<RectTransform>().pivot = new Vector2(1, .5f);
+			return new Vector3(-1*(float)radius*1.1f, (float)0, 0);
+		} 
+		else if (nearBases.N == null)
+		{
+			displayText.GetComponent<RectTransform>().pivot = new Vector2(.5f, 0);
+			return new Vector3((float)0, (float)radius*1.1f, 0);
+		} 
+		else if (nearBases.S == null) 
+		{
+			displayText.GetComponent<RectTransform>().pivot = new Vector2(.5f, 1);
+			return new Vector3((float)0, -1*(float)radius*1.1f, 0);
+		}
+		return new Vector3((float)radius*1.1f, (float)radius, 0);
+	}
+
+	private NESWBases getNESWBases(Base b)
+	{
+		NESWBases retVal = new NESWBases ();
+		foreach (BaseWrapper bw in baseWrappers) 
+		{
+			Base other = bw.baseObj.GetComponent<TouchBase>().b;
+			if (b.world.add (new Point(0, 1)).Equals(other.world))
+			{
+				retVal.N = other;
+			} else if (b.world.add (new Point(1, 0)).Equals(other.world))
+			{
+				retVal.E = other;
+			} else if (b.world.add (new Point(0, -1)).Equals(other.world))
+			{
+				retVal.S = other;
+			} else if (b.world.add (new Point(-1, 0)).Equals(other.world))
+			{
+				retVal.W = other;
+			}
+		}
+		return retVal;
+	}
+
+
+	class NESWBases
+	{
+		public Base N { get; set; }
+		public Base E { get; set; }
+		public Base S { get; set; }
+		public Base W { get; set; }
 	}
 }
--- Assets/scripts/Point.cs
+++ Assets/scripts/Point.cs
@@ -4,4 +4,26 @@ using System.Collections;
 public class Point {
 	public int x	{ get; set; }
 	public int y	{ get; set; }
+
+	public override bool Equals (object obj)
+	{
+		Point other = (Point)obj;
+		return this.x == other.x && this.y == other.y;
+	}
+
+	public Point add(Point p)
+	{
+		return new Point (p.x + this.x, p.y + this.y);
+	}
+
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point()
+	{
+
+	}
 }
