8393996c13a04a10c6f200adc4d004e7cad043ed
kmwhelan93 <kmw8sf@virginia.edu>
2015-02-20 16:02:08 -0500

    added gold update over time

--- Assets/scripts/UpdateGold.cs
+++ Assets/scripts/UpdateGold.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using LitJson;
 
 public class UpdateGold : MonoBehaviour {
 
+	public UpdateGold instance;
 	public GameObject goldText;
 
 	// Use this for initialization
 	void Start () {
+		instance = this;
 		StartCoroutine (syncGold ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Globals.gold = Globals.gold + Time.deltaTime * Globals.goldPerSec;
+		displayGold ();
 	}
 
 
@@ -21,7 +25,20 @@ public class UpdateGold : MonoBehaviour {
 	{
 		WWW request = new WWW (RequestService.baseUrl + "sync/gold");
 		yield return request;
-		int gold = int.Parse (request.text);
-		goldText.GetComponent<Text> ().text = gold + "";
+		GoldSync goldSync = JsonMapper.ToObject<GoldSync>(request.text);
+		Globals.gold = goldSync.gold;
+		Globals.goldPerSec = goldSync.goldPerSec;
+		displayGold ();
+	}
+
+	public void displayGold()
+	{
+		goldText.GetComponent<Text> ().text = (int)Globals.gold + "";
+	}
+
+	public class GoldSync {
+		public float gold { get; set; }
+		public float goldPerSec { get; set; }
+
 	}
 }
