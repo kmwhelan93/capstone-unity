c14d4d9613415a7bab0703cf7d5e1dae59b3c9be
cmaimon <cem6at@virginia.edu>
2015-04-20 00:17:05 -0400

    removed hasViewed bools from AttackResultObj

--- Assets/scripts/JSONObjects/AttackResultObj.cs
+++ Assets/scripts/JSONObjects/AttackResultObj.cs
@@ -6,11 +6,16 @@ public class AttackResultObj : MonoBehaviour {
 	public int attackId { get; set; }
 	public string winnerUsername { get; set; }
 	public int numUnitsLeft { get; set; }
-	public bool winnerHasViewed { get; set; }
-	public bool loserHasViewed { get; set; }
+	//public bool winnerHasViewed { get; set; }
+	//public bool loserHasViewed { get; set; }
+	
+	//public override string ToString ()
+	//{
+	//	return string.Format ("[AttackResultObj: attackId={0}, winnerUsername={1}, numUnitsLeft={2}, winnerHasViewed={3}, loserHasViewed={4}]", attackId, winnerUsername, numUnitsLeft, winnerHasViewed, loserHasViewed);
+	//}
 	
 	public override string ToString ()
 	{
-		return string.Format ("[AttackResultObj: attackId={0}, winnerUsername={1}, numUnitsLeft={2}, winnerHasViewed={3}, loserHasViewed={4}]", attackId, winnerUsername, numUnitsLeft, winnerHasViewed, loserHasViewed);
+		return string.Format ("[AttackResultObj: attackId={0}, winnerUsername={1}, numUnitsLeft={2}]", attackId, winnerUsername, numUnitsLeft);
 	}
 }
